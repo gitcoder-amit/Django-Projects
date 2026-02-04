41b1088aa0038b6d344574dace572ff2c0f8baad
gitcoder-amit <yadavamit14801@gmail.com>
2026-02-04 22:52:40 +0530

    added CustomUser model with login field as phone_number

--- project_authentication/accounts/models.py
+++ project_authentication/accounts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15)
-    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    username = None
+    phone_number = models.CharField(max_length=15, unique=True)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)  # image will be stored in root folder inside profile_images folder
     is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+
+    objects = CustomUserManager()
 
 
     def __str__(self):
